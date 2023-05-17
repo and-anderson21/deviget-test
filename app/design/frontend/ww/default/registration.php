9a514521af3afa669ff36757f8e58a964f79a537
Anderson Oliveira <anderson.oliveira@overdose.digital>
2023-05-17 18:24:58 -0300

    Adding ww_default theme

---
+++ app/design/frontend/ww/default/registration.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @package     ww/default
+ * @author      Anderson Oliveira
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+    'frontend/ww/default',
+    __DIR__
+);
